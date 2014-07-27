33ee9d9b179aa1104d9df726629d5f7b084a4d75
Shuping LIU <lsp.lhh@gmail.com>
2014-07-27 06:21:30 +0000

    add get a single issue method in githubissues.js

--- routes/post.js
+++ routes/post.js
@@ -2,18 +2,13 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var http = require('http');
-//var level = require('level');
 var path = require('path');
-//var issuesDB = level(path.join(__dirname, '../db/issues'));
+var githubIssues = require('../lib/githubissues');
 
 router.get('/:id', function(req, res) {
-    res.end(req.params.id);
+    githubIssues.get(req.params.id, function(issue) {
+        res.end(issue);
+    });
 });
-
-//router.get('/', function(req, res) {
-//    readAllIssues(function(issues)  {
-//        res.render('index', { issues: issues });
-//    });
-//});
 
 module.exports = router;
