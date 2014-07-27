62a25e140dec6dfc027a8a60005832cf844d6a57
Shuping LIU <lsp.lhh@gmail.com>
2014-07-27 12:44:09 +0000

    convert markdown to html in post display page

--- routes/post.js
+++ routes/post.js
@@ -3,10 +3,18 @@ var router = express.Router();
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
+var markdown = require('markdown').markdown;
 var githubIssues = require('../lib/githubissues');
 
 router.get('/:id', function(req, res) {
     githubIssues.get(req.params.id, function(issue) {
+//        var body = markdown.toHTML(issue.body);
+//        var post = {
+//            title: issue.title,
+//            body: body
+//        };
+        
+        issue.md = markdown.toHTML;
         res.render('post', issue);
     });
 });
