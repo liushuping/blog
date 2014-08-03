a04d17ca1f2bc8da1beb5c9aa0e10bfa28159756
Shuping LIU <lsp.lhh@gmail.com>
2014-08-03 05:30:20 +0000

    only show post title in index page

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
-var level = require('level');
-var path = require('path');
-var downsize = require('downsize');
 var githubIssues = require('../lib/githubissues');
 
 router.get('/', function(req, res) {
     githubIssues.getAll(function(issues)  {
         var title = 'Liushuping\'s Blog';
         
-        issues.map(function(issue) {
-            issue.body = downsize(issue.body, {characters: 300, append: '...'});
-        });
-
         res.render('index', { issues: issues, title: title });
     });
 });
