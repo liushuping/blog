a3b1e0dbc4e889506d59ef0d9e685fd01f8091dd
Shuping LIU <lsp.lhh@gmail.com>
2014-08-03 10:01:48 +0000

    add description meta in index.js

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,14 @@ var githubIssues = require('../lib/githubissues');
 router.get('/', function(req, res) {
     githubIssues.getAll(function(issues)  {
         var title = '高阶是对抽象的抽象';
-        
-        res.render('index', { issues: issues, title: title });
+	var description = '';        
+	var body = {
+	    issues: issues,
+	    title: title,
+	    description: '刘淑平的Blog，专注于前端程序开发，致力于提高代码质量。对JavaScript和C#以及函数式编程有浓厚兴趣，认为抽象是解决复杂软件问题的强有力工具。'
+        };
+
+        res.render('index', body);
     });
 });
 
