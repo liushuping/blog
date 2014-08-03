cdc851abe383a9c54ec3547835430af20deaf4d0
Shuping LIU <lsp.lhh@gmail.com>
2014-08-03 13:22:34 +0000

    add keywords for index page

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res) {
 	var body = {
 	    issues: issues,
 	    title: title,
+	    keywords: '刘淑平,liushuping,Blog,JavaScript,C#,程序,抽象',
 	    description: '刘淑平的Blog，专注于前端程序开发，致力于提高代码质量。对JavaScript和C#以及函数式编程有浓厚兴趣，认为抽象是解决复杂软件问题的强有力工具。'
         };
 
