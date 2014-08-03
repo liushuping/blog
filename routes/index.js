6ae824ead71a10a4127806dde354d04ebbb3538a
Shuping LIU <lsp.lhh@gmail.com>
2014-08-03 08:28:30 +0000

    update blog title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var githubIssues = require('../lib/githubissues');
 
 router.get('/', function(req, res) {
     githubIssues.getAll(function(issues)  {
-        var title = 'Liushuping\'s Blog';
+        var title = '高阶是对抽象的抽象';
         
         res.render('index', { issues: issues, title: title });
     });
