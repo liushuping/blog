7e241b224881a44c14fafd0a1d11c23fb47afa43
Shuping LIU <lsp.lhh@gmail.com>
2014-08-11 15:06:56 +0000

    add post-config.js

---
+++ posts/post-config.js
@@ -0,0 +1,31 @@
+{
+    publish: [
+	{
+	    id: 10,
+	    path: 'an-example-of-thinking-in-abstraction.md',
+	    tags: ['Abstraction', 'JavaScript'],
+	    slug:''
+        },
+	{
+	    id: 9,
+	    path: 'http-url-verb-usage-in-web-api.md',
+	    tags: ['REST', 'RESTful', 'RPC', 'HTTP', 'Web API', 'URL', 'HTTP Verb'],
+	    slug:''
+        },
+	{
+	    id: 2,
+	    path: 'beginners-javascript-preface.md',
+	    tags: ['JavaScript'],
+	    slug:'Beginner\'s JavaScript - Preface'
+        },
+	{
+	    id: 1,
+	    path: 'javascript-problem-with-a-colon.md',
+	    tags: ['JavaScript', 'Code Convention'],
+	    slug:''
+        }
+    ],
+
+    draft: [
+    ]
+}
