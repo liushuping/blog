c3b8df7b15b39d1ebf1b38de47a8932e993c3707
Shuping LIU <lsp.lhh@gmail.com>
2014-08-12 14:48:22 +0000

    refactoring errorhandlers.js

--- errorhandlers.js
+++ errorhandlers.js
@@ -1,27 +1,24 @@
 function handle(app) {
-    /// error handlers
 
-    // development error handler
-    // will print stacktrace
-    if (app.get('env') === 'development') {
+    function onerror(dev) {
         app.use(function(err, req, res, next) {
             res.status(err.status || 500);
             res.render('error', {
                 message: err.message,
-                error: err
+                error: dev ? err : {}
             });
         });
     }
 
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        onerror(true);
+    }
+
     // production error handler
     // no stacktraces leaked to user
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
-    });
+    onerror(false);
 }
 
 exports.handle = handle;
