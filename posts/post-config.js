b05a2bec8202e8b58795ab81cdd71c85a6e1817d
Shuping LIU <lsp.lhh@gmail.com>
2014-08-13 16:09:13 +0000

    update post-config.js

--- posts/post-config.js
+++ posts/post-config.js
@@ -1,31 +1,35 @@
-{
-    publish: [
-	{
+module.exports = {
+    publishes: [{
 	    id: 10,
 	    path: 'an-example-of-thinking-in-abstraction.md',
 	    tags: ['Abstraction', 'JavaScript'],
+	    created_on: '2014-07-03T13:52:43Z',
 	    slug:'抽象的思考(一个例子)'
-        },
-	{
+        }, {
 	    id: 9,
 	    path: 'http-url-verb-usage-in-web-api.md',
-	    tags: ['REST', 'RESTful', 'RPC', 'HTTP', 'Web API', 'URL', 'HTTP Verb'],
+	    tags: ['REST', 'RPC', 'HTTP', 'Web API', 'URL', 'HTTP Verb'],
+	    created_on: '2014-07-03T13:52:43Z',
 	    slug:'谈一谈Web-API的URL和HTTP-Verb的使用'
-        },
-	{
+        }, {
+	    id: 3,
+	    path: 'javascript-object-as-hashtable.md',
+	    tags: ['JavaScript'],
+	    created_on: '2014-08-13T23:00:00Z',
+        }, {
 	    id: 2,
 	    path: 'beginners-javascript-preface.md',
 	    tags: ['JavaScript'],
+	    created_on: '2014-08-13T23:52:43Z',
 	    slug:'Beginner\'s JavaScript - Preface'
-        },
-	{
+        }, {
 	    id: 1,
 	    path: 'javascript-problem-with-a-colon.md',
 	    tags: ['JavaScript', 'Code Convention'],
+	    created_on: '2014-07-03T13:52:43Z',
 	    slug:'JavaScript-一个分号的麻烦'
         }
     ],
 
-    draft: [
-    ]
+    draft: []
 }
